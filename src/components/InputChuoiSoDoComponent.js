1d144f4c5174ce1ac8bbaf3440b44b7d61842441
Admin <admin@Admins-MacBook-Pro.local>
2018-10-05 10:42:57 +0700

    test ios

--- src/components/InputChuoiSoDoComponent.js
+++ src/components/InputChuoiSoDoComponent.js
@@ -3,7 +3,8 @@ import {
     View,
     Text,
     StyleSheet,
-    TextInput
+    TextInput,
+    Platform
  } from 'react-native';
  import GlobalValue from '../data/GlobalValue';
 
@@ -19,16 +20,18 @@ import {
 
      render() {
          return (
-             <View>
+            <View style={{height:50, marginBottom:20}}>
                 <Text style={styles.text_title}>Số dò (ví dụ: 66 hoặc 68,86):</Text>
                 <TextInput
-                    style={{marginHorizontal:5}}
+                    style={{height:40, borderColor: 'gray', borderWidth: 1, marginHorizontal:5, paddingHorizontal:5}}
                     maxLength = {5} 
                     placeholder={'Nhập số cần dò'}
                     placeholderTextColor = {'grey'}
                     onChangeText = {(text)=>this.updateDataInput(text)}
-                    keyboardType='numeric'
-                    value = {this.state.chuoiSoDo}             
+                    keyboardType={Platform.OS === 'ios'?'numbers-and-punctuation':'default'}
+                    value = {this.state.chuoiSoDo}  
+                    fontSize={18}  
+                    underlineColorAndroid='rgba(0,0,0,0)'
                 />    
              </View>
          );
